7a98211daf627c0cead463eff2ad39fadb1f3fff
Phanindra-tw <v.tadikonda@thoughtworks.com>
2024-06-17 08:48:48 +0530

    [Phanindra] | Refactor : Update codebase to maintain consistency with other Repos (#4)
    * Upgrade package to fix vulnerability & Update package-lock file
    
    * Upgrade numerical in SHopping cart & test file
    
    * Delete Remove Cart functionality to maintain consistency across repos.
    
    * Rewrite tests to be consistent with other repos
    
    * Update test/model/ShoppingCart.spec.js
    
    Co-authored-by: Fernando Basso <FernandoBasso.br@gmail.com>
    
    * Update test/model/ShoppingCart.spec.js
    
    Co-authored-by: Fernando Basso <FernandoBasso.br@gmail.com>
    
    ---------
    
    Co-authored-by: Fernando Basso <FernandoBasso.br@gmail.com>

--- test/model/ShoppingCart.spec.js
+++ test/model/ShoppingCart.spec.js
@@ -4,66 +4,62 @@ import ShoppingCart from "../../src/model/ShoppingCart.js";
 
 describe("Shopping cart should checkout", () => {
 
-    it("Should calculate correct total and loyalty points for 10% discounted products", () => {
-        const customer = new Customer("Test customer");
+    let customer;
+    beforeEach(() => {
+        customer = new Customer("Test customer");
+    });
+
+    it("Should calculate correct total for 10% discounted products", () => {
         const products = [new Product(100, "DIS_10_TestProduct", "Test product")];
         const shoppingCart = new ShoppingCart(customer, products);
-        
+
         const order = shoppingCart.checkout();
 
         expect(order.totalPrice).toBe(90);
-        expect(order.loyaltyPoints).toBe(10);
     });
 
-    it("Should calculate correct total and loyalty points for 15% discounted products", () => {
-        const customer = new Customer("Test customer");
-        const products = [new Product(150, "DIS_15_TestProduct", "Test product")];
+    it("Should calculate correct loyalty points for 10% discounted products", () => {
+        const products = [new Product(100, "DIS_10_TestProduct", "Test product")];
         const shoppingCart = new ShoppingCart(customer, products);
 
         const order = shoppingCart.checkout();
 
-        expect(order.totalPrice).toBe(127.5);
         expect(order.loyaltyPoints).toBe(10);
     });
 
-    it("Should calculate correct total and loyalty points for non discounted products", () => {
-        const customer = new Customer("Test customer");
+    it("Should calculate correct total for 15% discounted products", () => {
+        const products = [new Product(100, "DIS_15_TestProduct", "Test product")];
+        const shoppingCart = new ShoppingCart(customer, products);
+
+        const order = shoppingCart.checkout();
+
+        expect(order.totalPrice).toBe(85);
+    });
+
+    it("Should calculate correct loyalty points for 15% discounted products", () => {
+        const products = [new Product(100, "DIS_15_TestProduct", "Test product")];
+        const shoppingCart = new ShoppingCart(customer, products);
+
+        const order = shoppingCart.checkout();
+
+        expect(order.loyaltyPoints).toBe(20 / 3);
+    });
+
+    it("Should calculate correct total for non discounted products", () => {
         const products = [new Product(100, "TestProduct", "Test product")];
         const shoppingCart = new ShoppingCart(customer, products);
 
         const order = shoppingCart.checkout();
 
         expect(order.totalPrice).toBe(100);
+    });
+
+    it("Should calculate correct loyalty points for non discounted products", () => {
+        const products = [new Product(100, "TestProduct", "Test product")];
+        const shoppingCart = new ShoppingCart(customer, products);
+
+        const order = shoppingCart.checkout();
+
         expect(order.loyaltyPoints).toBe(20);
-    });
-});
-
-describe("Shopping cart should modify products", () => {
-    it("Should add another product to the cart", () => {
-        const customer = new Customer("Test Customer");
-        const products = [new Product(100, "TestProductOne", "Test Product One")];
-        const shoppingCart = new ShoppingCart(customer, products);
-
-        shoppingCart.addProduct(new Product(200, "TestProductTwo", "Test Product Two"));
-
-        expect(shoppingCart.products).toEqual([
-            new Product(100, "TestProductOne", "Test Product One"),
-            new Product(200, "TestProductTwo", "Test Product Two")
-        ]);
-    });
-
-    it("Should remove a product from the cart", () => {
-        const customer = new Customer("Test Customer");
-        const products = [
-            new Product(100, "TestProductOne", "Test Product One"),
-            new Product(200, "TestProductTwo", "Test Product Two")
-        ];
-        const shoppingCart = new ShoppingCart(customer, products);
-
-        shoppingCart.removeProduct(new Product(200, "TestProductTwo", "Test Product Two"));
-
-        expect(shoppingCart.products).toEqual([
-            new Product(100, "TestProductOne", "Test Product One")
-        ]);
     });
 });
